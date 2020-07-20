d1e4c237b87d75b55e40da7b60100baa82010811
Nick Griffey <nicholasgriffey@gmail.com>
2020-07-20 15:12:41 -0400

    Add function to grab CDC data in CSV format

--- javascript/script.js
+++ javascript/script.js
@@ -5,4 +5,12 @@ function date_string() {
 	return today.toDateString()
 }
 
+function get_cdc_data() {
+	let request = new XMLHttpRequest()
+	// This throws a warning about synchronous requests being deprecated
+	request.open('GET','https://www.cdc.gov/covid-data-tracker/Content/CoronaViewJson_01/US_MAP_DATA.csv',false)
+	request.send()
+	return request.responseText
+}
+
 document.getElementById('facts-date').innerText += " " + date_string()
