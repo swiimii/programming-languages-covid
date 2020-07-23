f95c28fc59d5dc37fc7cf53b9ffe7b09b1ac8907
Nick Griffey <nicholasgriffey@gmail.com>
2020-07-23 01:08:49 -0400

    Fill in data in facts section

--- javascript/script.js
+++ javascript/script.js
@@ -30,11 +30,27 @@ function get_selected_region() {
 	return dropdown.options[dropdown.selectedIndex].value
 }
 
-function update_facts() {
+function get_facts(region, csv) {
+	if (csv[0][2] === region) {
+		return csv[0].slice(3)
+	} else {
+		return get_facts(region, csv.slice(1))
+	}
+}
+
+function update_facts(csv) {
 	let region = get_selected_region()
-	document.getElementById('fact-1').innerText = "You chose " + region
-	document.getElementById('fact-2').innerText = "You chose " + region
-	document.getElementById('fact-3').innerText = "You chose " + region
+	let facts = get_facts(region, csv)
+
+	let total_cases = facts[0]
+	let total_deaths = facts[1]
+	let deaths_per_100k = facts[2]
+	let cases_7_days = facts[3]
+	let cases_per_100k = facts[4]
+
+	document.getElementById('fact-1').innerText = "Since the start of the pandemic, there have been " + total_cases + " COVID-19 cases in " + region + ". " + total_deaths + " people have died from the disease."
+	document.getElementById('fact-2').innerText = "This means for every 100,000 people in " + region + ", " + cases_per_100k + " people have caught COVID-19, and " + deaths_per_100k + " have died."
+	document.getElementById('fact-3').innerText = "In the last 7 days alone, there have been " + cases_7_days + " cases in " + region + "."
 }
 
 document.getElementById('facts-date').innerText += " " + date_string()
