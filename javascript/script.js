e513641d5fc5793ae73b399f968cfa70d978041b
Nick Griffey <nicholasgriffey@gmail.com>
2020-07-24 16:44:47 -0400

    Change variables to consts

--- javascript/script.js
+++ javascript/script.js
@@ -6,7 +6,7 @@ function date_string() {
 }
 
 function get_cdc_data() {
-	let request = new XMLHttpRequest()
+	const request = new XMLHttpRequest()
 	// This throws a warning about synchronous requests being deprecated
 	request.open('GET','https://www.cdc.gov/covid-data-tracker/Content/CoronaViewJson_01/US_MAP_DATA.csv',false)
 	request.send()
@@ -14,19 +14,19 @@ function get_cdc_data() {
 }
 
 function parse_csv(csv_str) {
-	let rows = csv_str.split("\r\n").slice(3)
+	const rows = csv_str.split("\r\n").slice(3)
 	return rows.map(function (row) {return row.split(",")})
 }
 
 function create_dropdown_option(name, dropdown) {
-	let el = document.createElement("option")
+	const el = document.createElement("option")
 	el.textContent = name
 	el.value = name
 	dropdown.appendChild(el)
 }
 
 function get_selected_region() {
-	let dropdown = document.getElementById("region")
+	const dropdown = document.getElementById("region")
 	return dropdown.options[dropdown.selectedIndex].value
 }
 
@@ -39,14 +39,14 @@ function get_facts(region, csv) {
 }
 
 function update_facts(csv) {
-	let region = get_selected_region()
-	let facts = get_facts(region, csv)
+	const region = get_selected_region()
+	const facts = get_facts(region, csv)
 
-	let total_cases = facts[0]
-	let total_deaths = facts[1]
-	let deaths_per_100k = facts[2]
-	let cases_7_days = facts[3]
-	let cases_per_100k = facts[4]
+	const total_cases = facts[0]
+	const total_deaths = facts[1]
+	const deaths_per_100k = facts[2]
+	const cases_7_days = facts[3]
+	const cases_per_100k = facts[4]
 
 	document.getElementById('fact-1').innerText = "Since the start of the pandemic, there have been " + total_cases + " COVID-19 cases in " + region + ". " + total_deaths + " people have died from the disease."
 	document.getElementById('fact-2').innerText = "This means for every 100,000 people in " + region + ", " + cases_per_100k + " people have caught COVID-19, and " + deaths_per_100k + " have died."
@@ -55,9 +55,9 @@ function update_facts(csv) {
 
 document.getElementById('facts-date').innerText += " " + date_string()
 
-let cdc_data = get_cdc_data()
-let cdc_data_csv = parse_csv(cdc_data)
+const cdc_data = get_cdc_data()
+const cdc_data_csv = parse_csv(cdc_data)
 
-let dropdown = document.getElementById("region")
-let options = cdc_data_csv.map(function (x) {return x[2]})
+const dropdown = document.getElementById("region")
+const options = cdc_data_csv.map(function (x) {return x[2]})
 options.map(function (region) {create_dropdown_option(region, dropdown)})
