6669a6f3e3bbfbec550bbbebfbd18056b0f77a03
jnotor <44676573+jnotor@users.noreply.github.com>
2020-07-28 15:13:14 -0400

    Gogo gadget flask app (#7)
    * initial commit for using flask instead of eel
    
    * reorganized for flask hierarchy but left folders that arent mine intact for now
    
    Co-authored-by: John Notorgiacomo <notorgjv@mail.uc.edu>

---
+++ flask-app/flask_app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import datetime
+
+app = Flask(__name__)
+
+# NOTE: do not include number in question name since we're going to shuffle lol
+q_a_pairs = {'1: ' : ['a1', 'a2', 'a3', 'a4'],
+             '2: ' : ['a1', 'a2', 'a3', 'a4'],
+             '3: ' : ['a1', 'a2', 'a3', 'a4']}
+
+@app.route('/')
+def home():
+    return render_template('site.html')
+
+
+@app.route('/quiz')
+def quiz():
+    key_list = list(q_a_pairs.keys())
+
+    # psuedo randomize the key value pairs using modulu time
+    for key in q_a_pairs.keys():
+        pos = key_list.index(key) if key_list.index(key) else 1
+        new_pos = datetime.datetime.now().second % pos
+
+        if new_pos > len(key_list):
+            new_pos = len(key_list) - 1
+
+        key_list.insert(new_pos, key_list.pop(key_list.index(key)))
+
+    return render_template('quiz.html', questions=key_list, answer_options=q_a_pairs)
+
+
+@app.route('/quiz', methods=['POST'])
+def quiz_answers():
+    # NOTE: request.form just gets the info filled out; doesn't disallow not answering
+    if len(request.form.to_dict()) != len(q_a_pairs):
+        return '<a href="http://127.0.0.1:5000/quiz"><h1>Please try again. Not all questions answered</h1></a>'
+    # They've filled everything out, so lets send off the form dict to C++ for cookin
+    return '<h1>Answers: <u>'+str(request.form.to_dict())+'</u></h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- python/#README.txt
+++
@@ -1,9 +0,0 @@
-references:
-https://smallbusiness.chron.com/run-python-webpage-33100.html
-https://nitratine.net/blog/post/python-gui-using-chrome/
-https://realpython.com/python-web-applications/
-try this first^
-https://docs.python.org/2/howto/webservers.html
-https://stackoverflow.com/questions/47330200/how-to-make-javascript-and-python-work-together/47330433
-https://www.makeuseof.com/tag/python-javascript-communicate-json/
-sort of ^
