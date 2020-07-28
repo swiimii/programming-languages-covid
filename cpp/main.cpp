57983dd44ebaa1a53f35212b5a1885d935d45ac6
Sophia Keller <kellesm@mail.uc.edu>
2020-07-28 14:55:40 -0400

    make http request and save results example

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,9 +1,60 @@
 #include <iostream>
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+#include <cpprest/json.h>
 
 using namespace std;
+using namespace utility;
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
+using namespace cuncurrency::streams;
+using namespace web::json;
 
 int main()
 {
-    cout << "Hello world" << endl;
+	// HTTP request and saving results tutorial
+
+	auto fileStream = std::make_shared<ostream>();
+
+	// Open stream to output file.
+	pplx::task<void> requestTask = fstream::open_ostream(U("results.html")).then([=](ostream outFile)
+	{
+		*fileStream = outFile;
+
+		// Create http_client to send the request.
+		http_client client(U("http://www.bing.com/"));
+
+		// Build request URI and start the request.
+		uri_builder builder(U("/search"));
+		builder.append_query(U("q"), U("cpprestsdk github"));
+		return client.request(methods::GET, builder.to_string());
+	});
+
+	// Handle response headers arriving.
+	.then([=](http_response response)
+	{
+		printf("Received response status code:%u\n", response.status_code());
+
+		// Write response body into the file.
+		return response.body().read_to_end(fileStream->streambuf());
+	});
+
+	// Close the file stream.
+	.then([=](size_t)
+	{
+		return fileStream->close();
+	});
+
+	// Wait for all the outstanding I/O to complete and handle any exceptions
+	try
+	{
+		requestTask.wait();
+	}
+	catch (const std::exception & e)
+	{
+		printf("Error exception:%s\n", e.what());
+	}
+
     return 0;
 }
