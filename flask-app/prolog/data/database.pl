6cb514f92cd8eb740d718fdc952799518c668bd8
John Notorgiacomo <notorgjv@mail.uc.edu>
2020-08-05 13:29:26 -0400

    removed bug in database; reduced to 3 questions for integration with C++ rest

--- flask-app/prolog/data/database.pl
+++ flask-app/prolog/data/database.pl
@@ -1,17 +1,11 @@
 question(1,'What percentage of people who get COVID-19 will get very sick or die?').
-question(2,'You can always tell if someone has COVID-19 by:').
+question(2,'You can always tell if someone has COVID-19 by').
 question(3,'Will drinking very cold or very warm drinks stop you from getting COVID-19?').
-question(4, 'Is wearing a mask helpful for stoping the spread of COVID-19?')
-question(4, 'Is social distancing helpful for stoping the spread of COVID-19?')
 
 options(1, '~20%', '~50%', 'Everyone').
 options(2, 'Noticing their smell', 'Checking if they have any one of the symptoms', 'You cannot always tell').
-options(3, 'Yes, Hot', 'Yes, Cold', 'No').
-options(4, 'Yes', 'No', 'No, the government told me to do it, so it\'s dumb and inhibiting my rights.').
-options(5, 'Yes', 'No', 'No, the government told me to do it, so it\'s dumb and inhibiting my rights.').
+options(3, 'Yes - Hot', 'Yes - Cold', 'No').
 
 answer(1,'~20%').
 answer(2,'You cannot always tell').
 answer(3,'No').
-answer(4,'Yes').
-answer(5,'Yes').
